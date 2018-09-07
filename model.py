933f80d32dd45f48071dcba65b058421f8c6a243
Kartik N <kartikn@Kartiks-MacBook-Pro.local>
2018-09-06 18:55:01 -0700

    Added functions to normalize data and track predictions at each time step. LSTM cell operations are now functional

--- model.py
+++ model.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 
 
 train_ratio = 0.7
-tf.device('cpu')
 # Load data into dataframe
 # Drop unneeded dataframe columns
 df = pd.read_csv('bitcoin.csv').drop(['Volume_(Currency)','Weighted_Price','Volume_(BTC)'], axis=1)
@@ -21,12 +20,22 @@ test_end = dataset_size
 train = data[np.arange(train_start, train_end)]
 test = data[np.arange(test_start, test_end)]
 
-# Split into x and y train and test data
-x_train = train
-y_train = np.append(train[1:], train[-1])
+def normalize(list):
+    normalized_list = []
+    for i in range(len(list)):
+        if i == 0:
+            normalized_list.append(0)
+        else:
+            normalized_list.append((list[i]-list[i-1])/list[i-1])
+    return normalized_list
 
-x_test = test
-y_test = np.append(test[1:], test[-1])
+# Split into x and y train and test data
+x_train = normalize(train)
+y_train = np.append(x_train[1:], x_train[-1])
+
+x_test = normalize(test)
+y_test = np.append(x_test[1:], x_test[-1])
+
 
 #Tensorflow stuff
 
@@ -60,23 +69,45 @@ def tanh_layer(input, weight, bias):
     return tf.tanh(tf.add(tf.multiply(weight, input), bias))
 
 def lstm_cell(input, prev_out, prev_cell):
-    layer_op_input = [input, prev_out]
+    print("input: " + str(input))
+    # print("prev_out: " + str(prev_out))
+    # print("prev_cell: " + str(prev_cell))
+    layer_op_input = input+prev_out
+    # print("Layer op input: "+ str(layer_op_input))
+
     forget_layer = sigmoid_layer(layer_op_input, forget_weight, forget_bias)
+    # print("Forget layer " + str(forget_layer.eval()))
+
     input_layer = sigmoid_layer(layer_op_input, input_weight, input_bias)
+    # print("Input layer " + str(input_layer.eval()))
+
     candidate_layer = tanh_layer(layer_op_input, candidate_weight, candidate_bias)
+    # print("Candidate layer " + str(candidate_layer.eval()))
+
     output_layer = sigmoid_layer(layer_op_input, output_weight, output_bias)
+    # print("Output layer " + str(output_layer.eval()))
 
     # Values to update for next LSTM cell
     cell_state = tf.add(tf.multiply(forget_layer, prev_cell), tf.multiply(input_layer, candidate_layer))
+    print("Cell state: " + str(cell_state.eval()))
     output = tf.multiply(output_layer, tf.tanh(cell_state))
+    print("Output: " + str(output.eval()))
     # Update state and previous output
-    prev_cell_state = cell_state
-    prev_output = output
-    return output
+    return {'output': output.eval(), 'cell_state': cell_state.eval()}
 
 # Define loss function
 init = tf.global_variables_initializer()
+output_list = []
 with tf.Session() as sess:
     sess.run(init)
-    sess.run(lstm_cell(curr_input,prev_output,prev_cell_state),
-             feed_dict={curr_input: x_train})
+    for i in range(100):
+        print("Pass #" + str(i))
+        cell = lstm_cell(x_train[i], prev_output, prev_cell_state)
+        prev_output = cell['output']
+        output_list.append(prev_output)
+        prev_cell_state = cell['cell_state']
+
+plt.plot(output_list)
+plt.plot(x_train[0:100])
+plt.plot(np.zeros(100))
+plt.show()
